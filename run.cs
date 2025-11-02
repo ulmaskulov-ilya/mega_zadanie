c2eb26365e2c99423eb55c009a3ab24c738a24e0
ILYA <ylmaskylov-ilia@mail.ru>
2025-11-02 14:53:14 +0500

    check commit

---
+++ run.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int Solve(List<string> lines)
+    {
+        // TODO: Реализация алгоритма
+        return 0;
+    }
+
+    static void Main()
+    {
+        var lines = new List<string>();
+        string line;
+
+        while ((line = Console.ReadLine()) != null)
+        {
+            lines.Add(line);
+        }
+
+        int result = Solve(lines);
+        Console.WriteLine(result);
+    }
+}
